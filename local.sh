1455e3c054840fb83861929ac79b49c21d485225
Carlton N Hanna <nullpointer0x00@gmail.com>
2025-06-23 12:18:39 -0600

    update local.sh with deterministic accounts

--- local.sh
+++ local.sh
@@ -12,16 +12,19 @@ done
 
 if ! [ -f .vaulty/data/priv_validator_state.json ]; then
   simd init validator --chain-id "vaulty-1" --home .vaulty &> /dev/null
-
+  # provlabs19ftpcggezgal5ascglq5m022z4e453khv4j3k2
+  simd keys add account-1 --home .vaulty --keyring-backend test --recover <<< "picture pigeon chicken cause quarter script snow concert slim pill wedding oval vacant crew kingdom music ghost symbol sport poet velvet enhance warfare diet" &> /dev/null
+  # provlabs1evyv7neax9qtxxzuexnhylxyz4guvsyjhxyv47
+  simd keys add account-2 --home .vaulty --keyring-backend test --recover <<< "coyote online awkward talent share next hamster speed minimum color false amazing balcony treat squirrel feature combine negative fame decide usage mistake nurse begin" &> /dev/null
   simd keys add validator --home .vaulty --keyring-backend test &> /dev/null
   simd genesis add-genesis-account validator 1000000ustake --home .vaulty --keyring-backend test
-  simd genesis add-genesis-account provlabs17trszny8xuz2vn8vnqx6edt5c72nq4hprcxwcz 1000000uusdc --home .vaulty --keyring-backend test
-
+  simd genesis add-genesis-account provlabs19ftpcggezgal5ascglq5m022z4e453khv4j3k2 1000000uusdc --home .vaulty
+  simd genesis add-genesis-account provlabs1evyv7neax9qtxxzuexnhylxyz4guvsyjhxyv47 1000000uusdc --home .vaulty
   TEMP=.vaulty/genesis.json
   touch $TEMP && jq '.app_state.staking.params.bond_denom = "ustake"' .vaulty/config/genesis.json > $TEMP && mv $TEMP .vaulty/config/genesis.json
 
-  simd genesis gentx validator 1000000ustake --chain-id "vaulty-1" --home .vaulty --keyring-backend test 
-  simd genesis collect-gentxs --home .vaulty 
+  simd genesis gentx validator 1000000ustake --chain-id "vaulty-1" --home .vaulty --keyring-backend test &> /dev/null
+  simd genesis collect-gentxs --home .vaulty &> /dev/null 
 
   sed -i '' 's/timeout_commit = "5s"/timeout_commit = "1s"/g' .vaulty/config/config.toml
 fi

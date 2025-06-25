6444b731d471ba4fe47b5ca7db89dc0019c22540
Carlton N Hanna <nullpointer0x00@gmail.com>
2025-06-24 21:42:25 -0600

    add simple queries

--- keeper/query_server.go
+++ keeper/query_server.go
@@ -2,7 +2,9 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/provlabs/vault/types"
 )
 
@@ -18,12 +20,45 @@ func NewQueryServer(keeper *Keeper) types.QueryServer {
 
 // Vaults returns a paginated list of all vaults.
 func (k queryServer) Vaults(goCtx context.Context, req *types.QueryVaultsRequest) (*types.QueryVaultsResponse, error) {
-	panic("not implemented")
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	vaultMap, err := k.Keeper.GetVaults(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	vaults := make([]types.Vault, 0, len(vaultMap))
+	for _, v := range vaultMap {
+		vaults = append(vaults, v)
+	}
+
+	// TODO add pagination
+	return &types.QueryVaultsResponse{
+		Vaults: vaults,
+	}, nil
 }
 
 // Vault returns the configuration and state of a specific vault.
 func (k queryServer) Vault(goCtx context.Context, req *types.QueryVaultRequest) (*types.QueryVaultResponse, error) {
-	panic("not implemented")
+	if req == nil || req.VaultAddress == "" {
+		return nil, fmt.Errorf("vault_address must be provided")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	vaultAddr, err := sdk.AccAddressFromBech32(req.VaultAddress)
+	if err != nil {
+		return nil, fmt.Errorf("invalid vault_address: %w", err)
+	}
+
+	vault, found := k.Keeper.GetVaultByAddress(ctx, vaultAddr)
+	if !found {
+		return nil, fmt.Errorf("vault with address %q not found", req.VaultAddress)
+	}
+
+	return &types.QueryVaultResponse{
+		Vault: vault,
+	}, nil
 }
 
 // TotalAssets returns the total amount of the underlying asset managed by the vault.

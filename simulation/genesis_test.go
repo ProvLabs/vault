806a8ae47c9516f993ac8b45e7a2cc4103f9b13d
Carlton N Hanna <nullpointer0x00@gmail.com>
2025-06-30 08:16:46 -0600

    add export/init simulations

---
+++ simulation/genesis_test.go
@@ -0,0 +1,86 @@
+package simulation_test
+
+import (
+	"encoding/json"
+	"math/rand"
+	"testing"
+
+	sdkmath "cosmossdk.io/math"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+
+	"github.com/provlabs/vault/simulation"
+	"github.com/provlabs/vault/types"
+)
+
+func TestRandomizedGenState(t *testing.T) {
+	interfaceRegistry := codectypes.NewInterfaceRegistry()
+	cdc := codec.NewProtoCodec(interfaceRegistry)
+
+	s := rand.NewSource(1)
+	r := rand.New(s)
+
+	simState := module.SimulationState{
+		AppParams:    make(simtypes.AppParams),
+		Cdc:          cdc,
+		Rand:         r,
+		NumBonded:    3,
+		Accounts:     simtypes.RandomAccounts(r, 3),
+		InitialStake: sdkmath.NewInt(1000),
+		GenState:     make(map[string]json.RawMessage),
+	}
+
+	simulation.RandomizedGenState(&simState)
+
+	var vaultGenesis types.GenesisState
+	simState.Cdc.MustUnmarshalJSON(simState.GenState[types.ModuleName], &vaultGenesis)
+
+	require.Equal(t, types.Params{}, vaultGenesis.Params)
+	require.NotEmpty(t, vaultGenesis.Vaults)
+	for _, v := range vaultGenesis.Vaults {
+		require.NotEmpty(t, v.Admin)
+		require.NotEmpty(t, v.ShareDenom)
+		require.NotEmpty(t, v.GetAddress())
+		require.Len(t, v.UnderlyingAssets, 1)
+	}
+}
+
+func TestRandomizedGenState_Panics(t *testing.T) {
+	interfaceRegistry := codectypes.NewInterfaceRegistry()
+	cdc := codec.NewProtoCodec(interfaceRegistry)
+
+	s := rand.NewSource(1)
+	r := rand.New(s)
+
+	tests := []struct {
+		name     string
+		simState module.SimulationState
+	}{
+		{
+			name:     "nil simState",
+			simState: module.SimulationState{},
+		},
+		{
+			name: "missing GenState map",
+			simState: module.SimulationState{
+				AppParams: make(simtypes.AppParams),
+				Cdc:       cdc,
+				Rand:      r,
+				Accounts:  simtypes.RandomAccounts(r, 1),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Panics(t, func() {
+				simulation.RandomizedGenState(&tt.simState)
+			})
+		})
+	}
+}

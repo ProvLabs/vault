1b5991b8fc4b36e01e3b2dc8c5d52872a5529433
Carlton N Hanna <nullpointer0x00@gmail.com>
2025-07-24 10:51:21 -0600

    begin blocker initial impl

--- keeper/abci.go
+++ keeper/abci.go
@@ -2,9 +2,45 @@ package keeper
 
 import (
 	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k *Keeper) BeginBlocker(ctx context.Context) error {
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+	blockTime := sdkCtx.BlockTime().Unix()
+
+	iter, err := k.VaultInterestDetails.Iterate(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer iter.Close()
+
+	for ; iter.Valid(); iter.Next() {
+		vaultAddr, err := iter.Key()
+		if err != nil {
+			continue
+		}
+
+		details, err := iter.Value()
+		if err != nil {
+			continue
+		}
+
+		if details.ExpireTime <= blockTime {
+			vault, err := k.GetVault(sdkCtx, vaultAddr)
+			if err != nil {
+				continue
+			}
+			if vault == nil {
+				continue
+			}
+			if err := k.ReconcileVaultInterest(sdkCtx, vault); err != nil {
+				continue
+			}
+		}
+	}
+
 	return nil
 }
 

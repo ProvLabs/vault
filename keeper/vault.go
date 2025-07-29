cf35f3360fe46eb436074f926ffcdc372f231ff5
Carlton N Hanna <nullpointer0x00@gmail.com>
2025-07-29 09:34:50 -0600

    revert vault validate change, already done in createVaultAccount function

--- keeper/vault.go
+++ keeper/vault.go
@@ -47,11 +47,6 @@ func (k *Keeper) CreateVault(ctx sdk.Context, attributes VaultAttributer) (*type
 		return nil, fmt.Errorf("failed to create vault marker: %w", err)
 	}
 
-	err = vault.Validate()
-	if err != nil {
-		return nil, fmt.Errorf("failed to validate vault: %w", err)
-	}
-
 	k.emitEvent(ctx, types.NewEventVaultCreated(vault))
 
 	return vault, nil

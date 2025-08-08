09a8b391dec8af8664773cc6d9909145ed3d5b13
Carlton N Hanna <nullpointer0x00@gmail.com>
2025-08-08 13:18:07 -0600

    default to enabled swap in swap out

--- types/vault.go
+++ types/vault.go
@@ -52,8 +52,8 @@ func NewVaultAccount(baseAcc *authtypes.BaseAccount, admin string, shareDenom st
 		UnderlyingAssets:    underlyingAssets,
 		CurrentInterestRate: ZeroInterestRate,
 		DesiredInterestRate: ZeroInterestRate,
-		SwapInEnabled:       false, // TODO: should we default it to true or false?
-		SwapOutEnabled:      false,
+		SwapInEnabled:       true, // TODO: should we default it to true or false?
+		SwapOutEnabled:      true,
 	}
 }
 

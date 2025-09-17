787cd36bf590fe865c6b8ca2ffdf814e09a53e55
Matthew Witkowski <mwitkowski@provlabs.io>
2025-09-17 14:50:53 -0400

    Update the randomized genesis.

--- simulation/genesis.go
+++ simulation/genesis.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/provlabs/vault/keeper"
 	"github.com/provlabs/vault/types"
+	"github.com/provlabs/vault/utils"
 
 	sdkmath "cosmossdk.io/math"
 
@@ -16,99 +17,199 @@ import (
 
 // RandomizedGenState generates a random GenesisState for the vault module
 func RandomizedGenState(simState *module.SimulationState) {
-	admin := simState.Accounts[0].Address.String()
-	underlying := "underlying"
-	payment := ""
+	vaults := randomVaults(simState)
+	timeouts := randomTimeouts(simState, vaults)
 
-	vaults := []types.VaultAccount{}
-	timeouts := []types.QueueEntry{}
-	pendingSwaps := []types.PendingSwapOutQueueEntry{}
-	pendingSwapOutQueue := types.PendingSwapOutQueue{}
-	pendingSwapOutQueue.Entries = pendingSwaps
-	maxSequence := uint64(0)
+pendingSwaps := randomPendingSwaps(simState, vaults)
 
-	// TODO Make sure the vault account has the shares to burn
-	// TODO Make sure the MarkerAccount has enough of the asset
-	// TODO Make sure there is a Marker for the shares and has the sum of all shares + more
-	// TODO Make sure the vault account has enough of underlying asset for paying interest
-
-	for i := 0; i < simState.Rand.Intn(5)+1; i++ {
-		denom := fmt.Sprintf("vaultshare%d", i)
-		addr := types.GetVaultAddress(denom)
-
-		minRate := simState.Rand.Float64() * 0.1
-		maxRate := minRate + simState.Rand.Float64()*0.1
-		desiredRate := minRate + simState.Rand.Float64()*(maxRate-minRate)
-		currentRate := minRate + simState.Rand.Float64()*(maxRate-minRate)
-		minInterestRate := fmt.Sprintf("%f", minRate)
-		maxInterestRate := fmt.Sprintf("%f", maxRate)
-		desiredInterestRate := fmt.Sprintf("%f", desiredRate)
-		currentInterestRate := fmt.Sprintf("%f", currentRate)
-		periodStart := simState.GenTimestamp.Unix()
-		periodTimeout := simState.GenTimestamp.Unix() + simState.Rand.Int63n(int64(keeper.AutoReconcileTimeout))
-		swapInEnabled := simState.Rand.Intn(2) == 1
-		swapOutEnabled := simState.Rand.Intn(2) == 1
-		withdrawalDelaySeconds := uint64(simState.Rand.Int63n(keeper.AutoReconcileTimeout))
-
-		vaults = append(vaults, types.VaultAccount{
-			BaseAccount:            authtypes.NewBaseAccountWithAddress(addr),
-			Admin:                  admin,
-			ShareDenom:             denom,
-			UnderlyingAsset:        underlying,
-			PaymentDenom:           payment,
-			CurrentInterestRate:    currentInterestRate,
-			DesiredInterestRate:    desiredInterestRate,
-			MinInterestRate:        minInterestRate,
-			MaxInterestRate:        maxInterestRate,
-			PeriodStart:            periodStart,
-			PeriodTimeout:          periodTimeout,
-			SwapInEnabled:          swapInEnabled,
-			SwapOutEnabled:         swapOutEnabled,
-			WithdrawalDelaySeconds: withdrawalDelaySeconds,
-		})
-
-		timeouts = append(timeouts, types.QueueEntry{
-			Time: uint64(periodTimeout),
-			Addr: addr.String(),
-		})
-
-		// Create a random number of pending swaps for the vault
-		for j := 0; j < simState.Rand.Intn(11); j++ {
-			owner := simState.Accounts[simState.Rand.Intn(len(simState.Accounts))].Address
-			shares := sdk.NewCoin(denom, sdkmath.NewInt(simState.Rand.Int63n(1000)+1))
-			assets := sdk.NewCoin(underlying, sdkmath.NewInt(simState.Rand.Int63n(1000)+1))
-			requestID := uint64(i*10 + j)
-			payoutTime := simState.GenTimestamp.Unix() + int64(withdrawalDelaySeconds)
-
-			pendingSwaps = append(pendingSwaps, types.PendingSwapOutQueueEntry{
-				Time: payoutTime,
-				Id:   requestID,
-				SwapOut: types.PendingSwapOut{
-					VaultAddress: addr.String(),
-					Owner:        owner.String(),
-					Shares:       shares,
-					Assets:       assets,
-				}},
-			)
-			if requestID > maxSequence {
-				maxSequence = requestID
-			}
-		}
-	}
-
-	pendingSwapOutQueue.LatestSequenceNumber = maxSequence
+	// TODO: Fund all accounts and markers based on the generated state.
 
 	vaultGenesis := types.GenesisState{
 		Vaults:              vaults,
 		PayoutTimeoutQueue:  timeouts,
-		PendingSwapOutQueue: pendingSwapOutQueue,
+		PendingSwapOutQueue: pendingSwaps,
 	}
 
 	bz, err := json.MarshalIndent(&vaultGenesis, "", " ")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Selected randomly generated vault parameters: %s", bz)
+	fmt.Printf("Selected randomly generated vault parameters: %s\n", bz)
 
 	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(&vaultGenesis)
+}
+
+func randomVaults(simState *module.SimulationState) []types.VaultAccount {
+	const (
+		maxNumVaults            = 5
+		chanceOfPaymentDenom    = 2 // 1 in X
+		chanceOfNegativeMinRate  = 10 // 1 in X
+		maxPositiveMinRate      = 0.1
+		maxNegativeMinRate      = 0.05
+		maxRateAddition         = 0.2
+		chanceOfNewVault        = 3  // 1 in X
+		chanceOfPausedVault     = 5  // 1 in X
+		maxPausedBalance        = 1_000_000
+		minPausedBalance        = 1_000
+		chanceOfSwapInEnabled   = 2 // 1 in X
+		chanceOfSwapOutEnabled  = 2 // 1 in X
+	)
+
+	admin := simState.Accounts[0].Address.String()
+	var vaults []types.VaultAccount
+
+	for i := 0; i < simState.Rand.Intn(maxNumVaults)+1; i++ {
+		// --- Vault Config ---
+		shareDenom := fmt.Sprintf("vaultshare%d", i)
+		underlyingDenom := "underlying"
+		paymentDenom := ""
+		if simState.Rand.Intn(chanceOfPaymentDenom) == 0 {
+			paymentDenom = "payment"
+		}
+
+		addr := types.GetVaultAddress(shareDenom)
+
+		var minRate float64
+		if simState.Rand.Intn(chanceOfNegativeMinRate) == 0 {
+			minRate = (simState.Rand.Float64() - 1) * maxNegativeMinRate
+		} else {
+			minRate = simState.Rand.Float64() * maxPositiveMinRate
+		}
+
+		maxRate := minRate + simState.Rand.Float64()*maxRateAddition
+		desiredRate := minRate + simState.Rand.Float64()*(maxRate-minRate)
+		currentRate := minRate + simState.Rand.Float64()*(maxRate-minRate)
+		withdrawalDelaySeconds := uint64(simState.Rand.Int63n(keeper.AutoReconcileTimeout))
+
+		var periodStart, periodTimeout int64
+		if simState.Rand.Intn(chanceOfNewVault) == 0 {
+			periodStart = 0
+			periodTimeout = 0
+		} else {
+			periodStart = simState.GenTimestamp.Unix()
+			periodTimeout = simState.GenTimestamp.Unix() + simState.Rand.Int63n(int64(keeper.AutoReconcileTimeout))
+		}
+
+		var paused bool
+		var pausedBalance sdk.Coin
+		if simState.Rand.Intn(chanceOfPausedVault) == 0 {
+			paused = true
+			pausedBalance = sdk.NewCoin(underlyingDenom, sdkmath.NewInt(simState.Rand.Int63n(maxPausedBalance)+minPausedBalance))
+		} else {
+			paused = false
+			pausedBalance = sdk.Coin{}
+		}
+
+		vault := types.VaultAccount{
+			BaseAccount:            authtypes.NewBaseAccountWithAddress(addr),
+			Admin:                  admin,
+			ShareDenom:             shareDenom,
+			UnderlyingAsset:        underlyingDenom,
+			PaymentDenom:           paymentDenom,
+			CurrentInterestRate:    fmt.Sprintf("%f", currentRate),
+			DesiredInterestRate:    fmt.Sprintf("%f", desiredRate),
+			MinInterestRate:        fmt.Sprintf("%f", minRate),
+			MaxInterestRate:        fmt.Sprintf("%f", maxRate),
+			PeriodStart:            periodStart,
+			PeriodTimeout:          periodTimeout,
+			SwapInEnabled:          simState.Rand.Intn(chanceOfSwapInEnabled) == 0,
+			SwapOutEnabled:         simState.Rand.Intn(chanceOfSwapOutEnabled) == 0,
+			WithdrawalDelaySeconds: withdrawalDelaySeconds,
+			Paused:                 paused,
+			PausedBalance:          pausedBalance,
+		}
+		vaults = append(vaults, vault)
+	}
+
+	return vaults
+}
+
+func randomTimeouts(simState *module.SimulationState, vaults []types.VaultAccount) []types.QueueEntry {
+	const (
+		chanceOfTimeoutEntry = 2 // 1 in X
+	)
+
+	var timeouts []types.QueueEntry
+
+	for _, vault := range vaults {
+		// A vault with no timeout set should not be in the queue.
+		if vault.PeriodTimeout == 0 {
+			continue
+		}
+
+		if simState.Rand.Intn(chanceOfTimeoutEntry) == 0 {
+			addr, err := sdk.AccAddressFromBech32(vault.GetAddress().String())
+			if err != nil {
+				panic(err)
+			}
+			timeouts = append(timeouts, types.QueueEntry{
+				Time: uint64(vault.PeriodTimeout),
+				Addr: addr.String(),
+			})
+		}
+	}
+
+	return timeouts
+}
+
+func randomPendingSwaps(simState *module.SimulationState, vaults []types.VaultAccount) types.PendingSwapOutQueue {
+	const (
+		maxNumPendingSwaps           = 11
+		maxSharesAmount              = 1000 // This is the base amount, it will be multiplied by ShareScalar
+		chanceOfRedeemDenomIsPayment = 2 // 1 in X
+		numPayoutTimeOptions         = 3
+	)
+
+	var allPendingSwaps []types.PendingSwapOutQueueEntry
+	maxSequence := uint64(0)
+
+	for _, vault := range vaults {
+		numPendingSwaps := simState.Rand.Intn(maxNumPendingSwaps)
+		for j := 0; j < numPendingSwaps; j++ {
+			maxSequence++
+			requestID := maxSequence
+
+			owner := simState.Accounts[simState.Rand.Intn(len(simState.Accounts))].Address
+			
+			// Multiply the base random amount by the ShareScalar to get a realistic, scaled share amount
+			baseSharesAmount := sdkmath.NewInt(simState.Rand.Int63n(maxSharesAmount) + 1)
+			scaledSharesAmount := baseSharesAmount.Mul(utils.ShareScalar)
+			shares := sdk.NewCoin(vault.ShareDenom, scaledSharesAmount)
+
+			redeemDenom := vault.UnderlyingAsset
+			if vault.PaymentDenom != "" && simState.Rand.Intn(chanceOfRedeemDenomIsPayment) == 0 {
+				redeemDenom = vault.PaymentDenom
+			}
+
+			var payoutTime int64
+			switch simState.Rand.Intn(numPayoutTimeOptions) {
+			case 0:
+				payoutTime = 0
+			case 1:
+				payoutTime = simState.GenTimestamp.Unix()
+			default:
+				if vault.WithdrawalDelaySeconds > 0 {
+					payoutTime = simState.GenTimestamp.Unix() + simState.Rand.Int63n(int64(vault.WithdrawalDelaySeconds))
+				} else {
+					payoutTime = simState.GenTimestamp.Unix()
+				}
+			}
+
+			allPendingSwaps = append(allPendingSwaps, types.PendingSwapOutQueueEntry{
+				Time: payoutTime,
+				Id:   requestID,
+				SwapOut: types.PendingSwapOut{
+					VaultAddress: vault.GetAddress().String(),
+					Owner:        owner.String(),
+					Shares:       shares,
+					RedeemDenom:  redeemDenom,
+				},
+			})
+		}
+	}
+
+	return types.PendingSwapOutQueue{
+		Entries:            allPendingSwaps,
+		LatestSequenceNumber: maxSequence,
+	}
 }

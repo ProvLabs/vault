206138db765e02d9093392ab705be96094a2db46
Carlton N Hanna <nullpointer0x00@gmail.com>
2025-10-01 14:54:37 -0600

    update reconcile tests to write navs

--- keeper/reconcile_test.go
+++ keeper/reconcile_test.go
@@ -17,6 +17,7 @@ func (s *TestSuite) TestKeeper_ReconcileVaultInterest() {
 	twoMonths := -24 * 60 * time.Hour
 	shareDenom := "vaultshares"
 	underlying := sdk.NewInt64Coin("underlying", 1_000_000_000)
+	totalShares := sdk.NewInt64Coin(shareDenom, 1_000_000_000_000_000)
 	vaultAddress := types.GetVaultAddress(shareDenom)
 	testBlockTime := time.Now()
 	futureTime := testBlockTime.Add(100 * time.Second)
@@ -37,11 +38,14 @@ func (s *TestSuite) TestKeeper_ReconcileVaultInterest() {
 		vault.DesiredInterestRate = interestRate
 		vault.PeriodStart = periodStartSeconds
 		vault.Paused = paused
+		vault.TotalShares = totalShares
 		s.k.AuthKeeper.SetAccount(s.ctx, vault)
+
 		err = FundAccount(s.ctx, s.simApp.BankKeeper, vaultAddress, sdk.NewCoins(underlying))
 		s.Require().NoError(err)
 		err = FundAccount(s.ctx, s.simApp.BankKeeper, markertypes.MustGetMarkerAddress(shareDenom), sdk.NewCoins(underlying))
 		s.Require().NoError(err)
+
 		s.ctx = s.ctx.WithBlockTime(testBlockTime)
 		s.ctx = s.ctx.WithEventManager(sdk.NewEventManager())
 	}
@@ -84,7 +88,25 @@ func (s *TestSuite) TestKeeper_ReconcileVaultInterest() {
 				s.assertInPayoutVerificationQueue(vaultAddress, true)
 				s.assertVaultAndMarkerBalances(vaultAddress, shareDenom, underlying.Denom, sdkmath.NewInt(958047987), sdkmath.NewInt(1041952013))
 			},
-			expectedEvents: createReconcileEvents(vaultAddress, markertypes.MustGetMarkerAddress(shareDenom), sdkmath.NewInt(41952013), sdkmath.NewInt(1_000_000_000), sdkmath.NewInt(1041952013), underlying.Denom, "0.25", 5_184_000),
+			expectedEvents: func() sdk.Events {
+				ev := createReconcileEvents(
+					vaultAddress,
+					markertypes.MustGetMarkerAddress(shareDenom),
+					sdkmath.NewInt(41952013),
+					sdkmath.NewInt(1_000_000_000),
+					sdkmath.NewInt(1_041_952_013),
+					underlying.Denom,
+					"0.25",
+					5_184_000,
+				)
+				nav := createMarkerSetNAV(
+					shareDenom,
+					sdk.NewCoin(underlying.Denom, sdkmath.NewInt(1_041_952_013)),
+					"vault",
+					totalShares.Amount.Uint64(),
+				)
+				return append(ev, nav)
+			}(),
 		},
 		{
 			name: "interest period has elasped, should pay negative interest and update period start",
@@ -93,9 +115,27 @@ func (s *TestSuite) TestKeeper_ReconcileVaultInterest() {
 			},
 			posthander: func() {
 				s.assertInPayoutVerificationQueue(vaultAddress, true)
-				s.assertVaultAndMarkerBalances(vaultAddress, shareDenom, underlying.Denom, sdkmath.NewInt(1040262904), sdkmath.NewInt(959737096))
+				s.assertVaultAndMarkerBalances(vaultAddress, shareDenom, underlying.Denom, sdkmath.NewInt(1_040_262_904), sdkmath.NewInt(959_737_096))
 			},
-			expectedEvents: createReconcileEvents(vaultAddress, markertypes.MustGetMarkerAddress(shareDenom), sdkmath.NewInt(-40262904), sdkmath.NewInt(1_000_000_000), sdkmath.NewInt(959737096), underlying.Denom, "-0.25", 5_184_000),
+			expectedEvents: func() sdk.Events {
+				ev := createReconcileEvents(
+					vaultAddress,
+					markertypes.MustGetMarkerAddress(shareDenom),
+					sdkmath.NewInt(-40_262_904),
+					sdkmath.NewInt(1_000_000_000),
+					sdkmath.NewInt(959_737_096),
+					underlying.Denom,
+					"-0.25",
+					5_184_000,
+				)
+				nav := createMarkerSetNAV(
+					shareDenom,
+					sdk.NewCoin(underlying.Denom, sdkmath.NewInt(959_737_096)),
+					"vault",
+					totalShares.Amount.Uint64(),
+				)
+				return append(ev, nav)
+			}(),
 		},
 		{
 			name: "paused vault, should do nothing",
@@ -106,7 +146,7 @@ func (s *TestSuite) TestKeeper_ReconcileVaultInterest() {
 				s.assertInPayoutVerificationQueue(vaultAddress, false)
 				vault, err := s.k.GetVault(s.ctx, vaultAddress)
 				s.Require().NoError(err)
-				s.Require().Equal(pastTime.Unix(), vault.PeriodStart, "PeriodStart should not be updated for paused vault")
+				s.Require().Equal(pastTime.Unix(), vault.PeriodStart)
 			},
 			expectedEvents: sdk.Events{},
 		},
@@ -120,7 +160,7 @@ func (s *TestSuite) TestKeeper_ReconcileVaultInterest() {
 			}
 
 			vault, err := s.k.GetVault(s.ctx, vaultAddress)
-			s.Require().NoError(err, "failed to get vault for test setup")
+			s.Require().NoError(err)
 			err = s.k.ReconcileVaultInterest(s.ctx, vault)
 
 			if tc.posthander != nil {
